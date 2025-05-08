1ab45acf2267d0fbfe273eb604ac366be08d677d
adarshvishwakarmaa <adarshvishwakarma2004@gmail.com>
2025-05-08 20:06:28 +0530

    add cors origin url in env

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: process.env.CORS_ORIGIN_URL,
     credentials:true
 }))
 
